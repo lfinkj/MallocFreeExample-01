c711d8a46437d87276ada564e9c65c5bb0e6b649
Lloyd Finckbone Jr <76671331+lfinkj@users.noreply.github.com>
2020-12-27 22:09:20 -0600

    Add files via upload

---
+++ malloc-free-01.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mallocArray()
+{
+	int n = 5;
+	int *a = (int*) malloc( n*sizeof(int) );
+	printf("malloc uninitialized: \n");
+	for( int i = 0; i < n; i++ )
+	{
+		printf("%d : %u\n", a[i], &a[i] );
+	}
+	for( int i = 0; i < n; i++ )
+	{
+		a[i] = i + 1;
+	}
+	printf("malloc 1++ loop initialized: \n");
+	for( int i = 0; i < n; i++ )
+	{
+		printf("%d : %u\n", a[i], &a[i] );
+	}
+	free( a );
+	printf("malloc array memory after free()\n");
+	for( int i = 0; i < n; i++ )
+	{
+		printf("%d : %u\n", a[i], &a[i] );
+	}
+}
+
+int main()
+{
+
+	mallocArray();
+
+
+	return 0;
+}
